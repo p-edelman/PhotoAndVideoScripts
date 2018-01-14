f3b6ea90425b029698537434e505fff4b29938ba
Pieter Edelman <p.edelman@gmail.com>
2018-01-14 11:07:32 +0100

    Found a bunch of other datetime tags that are relevant for movie files and that are ignored now. To properly include them, the whole datetime handling is delegated to a dedicated class.

--- correctphotodrift.py
+++ correctphotodrift.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, datetime, os.path, re, subprocess
+import argparse, datetime, os.path, re, subprocess, sys
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
@@ -9,6 +9,115 @@ class TimePoint:
     self.exif = exif
     self.real = real
 
+class MetaDataDateTime:
+  known_tags = [
+    "DateTimeOriginal",
+    "CreateDate",
+    "MediaCreateDate",
+    "MediaModifyDate",
+    "TrackCreateDate",
+    "TrackModifyDate"]
+
+  def __init__(self, path, ignore_read_tags = []):
+    self._path = path
+    
+    # The list of datetime related tags that we have in the metadata
+    self._tags = []
+    
+    # The datetime of the image as a seconds since epoch float
+    self._original_dt  = None
+    self._corrected_dt = None
+    
+    # Read the metadata
+    self.dt_tag = self._readMetaData(ignore_read_tags)
+
+  def _readMetaData(self, ignore_read_tags):
+    """ Read the relevant metadata from the image file. This sets the 
+        self._datatime and self._tags parameters.
+        It returns the name of the tag that was used for deterimining the
+        datetime. """
+
+    # Construct the command arguments
+    cmd = ["exiftool", "-veryShort", "-d", DATE_FORMAT]
+    for tag in self.known_tags: # Add every possible date tag
+      cmd.append("-%s" % tag)
+    cmd.append(self._path)
+
+    # Run exiftool
+    result = subprocess.run(cmd, stdout = subprocess.PIPE)
+    if result.returncode != 0:
+      raise Exception("Exiftool failed on %s" % self._path)
+    stdout = result.stdout.decode("utf-8")
+    
+    # Analyze the result
+    used_tag = None
+    for tag in self.known_tags:
+      if tag in stdout:
+        self._tags.append(tag)
+        
+        # Extract the datetime if we don't have it yet
+        if not self._original_dt and tag not in ignore_read_tags:
+          match = re.search("%s:\s+(.*)\s*" % tag, stdout)
+          if match:
+            used_tag = tag
+            self._original_dt = parseDateTime(match.group(1))
+    
+    if not self._original_dt:
+      raise Exception("Couldn't read date and time information from %s" % self._path)
+
+    return used_tag
+    
+  def calcCorrection(self, reference_points):
+    """ Calculate the corrected the datetime based on the list of reference
+        points. """
+    
+    # Find two points to use for interpolation. If the timestamp lies before or
+    # after the range, use the two nearest points.
+    if self._original_dt <= reference_points[0].exif:
+      start = reference_points[0]
+      end   = reference_points[1]
+    elif self._original_dt >= reference_points[-1].exif:
+      start = reference_points[-2]
+      end   = reference_points[-1]
+    else:
+      for i in range(len(reference_points) - 1):
+        if self._original_dt >= reference_points[i].exif and \
+           self._original_dt < reference_points[i + 1].exif:
+          start = reference_points[i]
+          end   = reference_points[i + 1]
+          break
+    
+    # Find the slope and offset to map an exif time on real time
+    slope  = (end.real - start.real) / (end.exif - start.exif)
+    offset = start.real - start.exif * slope
+    
+    # Use these values to calculate the correct time
+    self._corrected_dt = round(self._original_dt * slope + offset)
+    
+    return self._corrected_dt - self._original_dt
+    
+  def writeMetaData(self):
+    """ Write the corrected datetime to the metadata. Return True on success,
+        False on failure.
+        NOTE: you need to run calcCorrection() first. """
+        
+    if not self._corrected_dt:
+      raise Exception("You need to run the calcCorrection() method first!")
+
+    # Construct the Exiftool command by setting all known tags to the new stamp
+    cmd = ["exiftool", "-d", DATE_FORMAT]
+    dt_str = datetime.datetime.fromtimestamp(self._corrected_dt).strftime(DATE_FORMAT)
+    for tag in self._tags:
+      cmd.append("-%s=%s" % (tag, dt_str))
+    cmd.append(self._path)
+
+    # Run Exiftool
+    result = subprocess.run(cmd, stdout = subprocess.PIPE)
+    if result.returncode == 0:
+      return True
+    
+    return False
+    
 def parseDateTime(datetime_string):
   dt = datetime.datetime.strptime(datetime_string, DATE_FORMAT)
   return dt.timestamp()
@@ -33,68 +142,34 @@ def readCSVFile(path):
   
   return time_points
   
-def correctTimestamp(timestamp, time_points):
-  # Find two points to use for interpolation. If the timestamp lies before or
-  # after the range, use the two nearest points.
-  if timestamp <= time_points[0].exif:
-    start = time_points[0]
-    end   = time_points[1]
-  elif timestamp >= time_points[-1].exif:
-    start = time_points[-2]
-    end   = time_points[-1]
-  else:
-    for i in range(len(time_points) - 1):
-      if timestamp >= time_points[i].exif and timestamp < time_points[i + 1].exif:
-        start = time_points[i]
-        end   = time_points[i + 1]
-        break
-  
-  # Find the slope and offset to map an exif time on real time
-  slope  = (end.real - start.real) / (end.exif - start.exif)
-  offset = start.real - start.exif * slope
-  
-  # Use these values to calculate the correct time
-  return datetime.datetime.fromtimestamp(round(timestamp * slope + offset))
-
-def processPhoto(photo_path, time_points, possible_tags, dry_run = False):
-  """ Correct the time stamp of the photo specified at photo_path, using the
-      time_points tuple to interpolate to the correct time and the tag names in
-      the possible_tags array to read the metadata value from the file.
-      A report will be printed to stdout.
+def processPhoto(photo_path, reference_points, ignore_read_tags = [], dry_run = False):
+  """ Correct the datetime stamp of the photo specified at photo_path, using the
+      reference_points list of tuples to interpolate to the correct time.
+      The results will be printed to stdout / stderr.
+      The ignore_read_tags is a list of tags that shouldn't be used for
+      determining the datetime stamp of the photo.
       If dry_run is True, the photo isn't actually modified. """
       
   if not os.path.exists(photo_path):
     raise Exception("Photo file %s does not exist" % photo_path)
   
-  # Run exiftool to extract the timestamp. We try both the DateTimeOriginal and
-  # CreateDate tags, because one of them might be absent
-  exif = None
-  for tag in possible_tags:
-    result = subprocess.run(["exiftool", "-veryShort", "-%s" % tag, "-d", DATE_FORMAT, photo_path], stdout = subprocess.PIPE)
-    if result.returncode != 0:
-      raise Exception("Exiftool failed on %s" % photo_path)
-  
-    # Parse the result
-    match = re.match("%s:\s+(.*)\s*" % tag, result.stdout.decode("utf-8"))
-    if match:
-      exif = parseDateTime(match.group(1))
-      break
-  
-  if not exif:
-    raise Exception("DateTime couldn't be extracted from %s" % photo_path)
+  try:
+    metadata = MetaDataDateTime(photo_path, ignore_read_tags)
+  except:
+    sys.stdout.write("The date and time couldn't be extracted from %s" % photo_path)
+    sys.exit(1)
   
   # Calculate the correct time
-  corrected = correctTimestamp(exif, time_points)
+  diff = metadata.calcCorrection(reference_points)
   
   # Write the corrected time to the photo file
   if not dry_run:
-    result = subprocess.run(["exiftool", "-alldates=" + corrected.strftime(DATE_FORMAT), "-d", DATE_FORMAT, photo_path], stdout = subprocess.PIPE)
-    if result.returncode == 0:
-      print("Shifted %s (from %s tag) by %+.0f seconds" % (photo_path, tag, corrected.timestamp() - exif))
+    if metadata.writeMetaData():
+      print("Shifted %s (from %s tag) by %+.0f seconds" % (photo_path, metadata.dt_tag, diff))
     else:
       print("Error with %s" % photo_path)
   else:
-    print("%s will be shifted (from %s tag) by %+.0f seconds" % (photo_path, tag, corrected.timestamp() - exif))
+    print("%s will be shifted (from %s tag) by %+.0f seconds" % (photo_path, metadata.dt_tag, diff))
   
 if __name__ == "__main__":
   # Build a parser and parse the command line
@@ -102,10 +177,10 @@ if __name__ == "__main__":
   parser.add_argument("-n", "--dry-run", action = "store_true",
                       help = "Don't alter any files, just print out what would be done")
   tag_group = parser.add_mutually_exclusive_group()
-  tag_group.add_argument("-d", "--no-datetimeoriginal", dest = "datetimeoriginal", action = "store_false",
-                         help = "Don't use the DateTimeOriginal tag to determine photo date and time")
-  tag_group.add_argument("-c", "--no-createdate", dest = "createdate", action = "store_false",
-                         help = "Don't use the CreateDate tag to determine photo date and time")
+  tag_group.add_argument("-i", "--ignore-reading",
+                         choices = MetaDataDateTime.known_tags,
+                         action = "append",
+                         help = "Ignore this tag for reading the datetime (it will be included when writing though). This tag can be used multiple times.")
   parser.add_argument('csv_file', type = str, help = "The CSV file with the samples. It should contain rows with exif and real time, seperated by a comma, both in format \"yyyy-mm-dd hh:mm:ss\"")
   parser.add_argument('photo', type = str, nargs = "+")
   
@@ -119,10 +194,8 @@ if __name__ == "__main__":
   if status.returncode != 0:
     raise Exception("Exiftool can't be run")
   
-  time_points = readCSVFile(args.csv_file)
+  reference_points = readCSVFile(args.csv_file)
 
-  possible_tags = []
-  if args.datetimeoriginal: possible_tags.append("DateTimeOriginal")
-  if args.createdate:       possible_tags.append("CreateDate")
+  ignore_read_tags = args.ignore_reading if args.ignore_reading else []
   for photo in args.photo:
-    processPhoto(photo, time_points, possible_tags, args.dry_run)
+    processPhoto(photo, reference_points, ignore_read_tags, args.dry_run)
